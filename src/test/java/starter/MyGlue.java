c1a283c64be47b02cdaf6b43055e7839153e5154
John Ferguson Smart <john.smart@wakaleo.com>
2024-12-19 21:17:40 +0000

    Simplified the code

--- src/test/java/starter/MyGlue.java
+++ src/test/java/starter/MyGlue.java
@@ -1,50 +1,15 @@
 package starter;
 
-import static java.lang.String.join;
-import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
-import net.serenitybdd.core.exceptions.IgnoreStepException;
 import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.Consequence;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import net.serenitybdd.screenplay.rest.interactions.Get;
 import org.junit.jupiter.api.Assertions;
-import org.opentest4j.AssertionFailedError;
 
-import java.util.Arrays;
-import java.util.List;
+import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
 
 public class MyGlue {
-
-  class MyActor extends Actor {
-    public MyActor(String name) {
-      super(name);
-    }
-
-    public void myShould(Consequence<?>... consequences) {
-      for (Consequence<?> consequence : consequences) {
-        check(consequence);
-      }
-      throwSummaryExceptionFrom();
-    }
-
-    private void throwSummaryExceptionFrom() {
-      throw new AssertionFailedError("Oh crap!");
-    }
-
-    private <T> void check(Consequence<T> consequence) {
-
-//      ConsequenceCheckReporter reporter = new ConsequenceCheckReporter(eventBusInterface, consequence);
-      try {
-//        reporter.startQuestion(this);
-          consequence.evaluateFor(this);
-      } catch (IgnoreStepException e) {
-      }
-    }
-
-  }
 
   @Given("prepare something")
   public void prepare_something() {
@@ -59,15 +24,9 @@ public class MyGlue {
   @When("do something wrong")
   public void do_something_wrong() {
     Actor actor = Actor.named("Alice").whoCan(CallAnApi.at("http://www.example.com"));
-//    MyActor actor = new MyActor("Alice");
     actor.can(CallAnApi.at("http://www.example.com"));
     sendRequest(actor);
-//    actor.myShould(seeThatResponse("message", response -> response.statusCode(500)));
     actor.should(seeThatResponse("message", response -> response.statusCode(500)));
-  }
-
-  void fail() {
-    Assertions.assertTrue(false);
   }
 
   private <T extends Actor> void sendRequest(T actor) {
